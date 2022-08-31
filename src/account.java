7215e909d95c644e2cd26e79039c1b2afc1b1571
Ryan Pinsker <cjumper555@gmail.com>
2022-08-30 18:16:45 -0700

    made account constructor

---
+++ src/account.java
@@ -0,0 +1,12 @@
+public class account {
+private int accountID;
+private double balance;
+private boolean isOpen;
+
+public account(int accountID) {
+	this.accountID = accountID;
+	this.balance = 0;
+	this.isOpen = true;
+}
+}
+
